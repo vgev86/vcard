053a820004a5030d15e6953977666465154664fd
Vardan Gevorgyan <75502111+vgev86@users.noreply.github.com>
2024-02-05 22:02:42 +0400

    datapart

--- admin_functions.php
+++ admin_functions.php
@@ -8,7 +8,7 @@ function handleFormSubmission() {
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Check connection
+    
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }

86890a1046ba99a382a8dd89f4d423d89d0501eb
Vardan Gevorgyan <75502111+vgev86@users.noreply.github.com>
2024-02-05 21:07:40 +0400

    name

--- admin.php
+++ admin.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/admin.css">
-</center><title>Admin Panel</title><center>
+</center><title>vCard Admin Panel</title><center>
 </head>
 <body>
     <div class="container">

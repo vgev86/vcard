8eeae1d666cec9e27d730338a7897644aba2fc8e
vgev86 <75502111+vgev86@users.noreply.github.com>
2024-02-21 19:20:28 +0400

    v2 start

--- qr_functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-// qr_functions.php
-require 'lib/phpqrcode/qrlib.php';
-
-function generateQRCode($text) {
-    // Generate QR code
-    QRcode::png($text);
-}
-?>
--- register.php
+++
@@ -1,6 +0,0 @@
-<!-- register.php -->
-<form action="register_process.php" method="post">
-    Username: <input type="text" name="username" required><br>
-    Password: <input type="password" name="password" required><br>
-    <input type="submit" value="Register">
-</form>

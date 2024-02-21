ad1d44f2ea6ae6b92020e92b4a4c09ab345e5dfb
vgev86 <75502111+vgev86@users.noreply.github.com>
2024-02-21 17:24:08 +0400

    back

--- index.php
+++ index.php
@@ -3,18 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>vCard Website</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>vCard - ACTrade</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <div class="logo-container">
-        <img src="/data/logo.png" alt="Logo">
-    </div>
-    <div class="button-container">
-        <button onclick="location.href='user_panel.php'">User Panel</button>
-        <button onclick="location.href='registration_form.php'">Registration Form</button>
-        <button onclick="location.href='generate_qr_code.php'">Generate QR Code</button>
-    </div>
+    <header>
+        <img src="files/logo.png" alt="vCard Logo">
+        <nav>
+         <a href="generateqr.php" class="button">Generate QR Code</a>
+            <form action="userinfo.php" method="GET">
+                <input type="text" name="id" placeholder="Search by ID">
+                <button type="submit" class="button">Search</button>
+            </form>
+            <a href="register.php" class="button">Register</a>
+            <a href="login.php" class="button">Login</a>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <h1>Welcome to vCard</h1>
+            <p>This is a platform for managing and sharing digital business cards.</p>
+        </div>
+    </main>
+    <footer>
+        <p>&copy; 2024 vCard - ACTrade</p>
+    </footer>
 </body>
 </html>
-

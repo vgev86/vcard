c340e6433c085df6b40782b9a7cd73540ec51ef9
vgev86 <75502111+vgev86@users.noreply.github.com>
2024-02-23 09:43:46 +0400

    beeon

--- user_functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-function getUserInformationById($userId) {
-    // Implement logic to retrieve user information based on the ID
-    // Use $userId in your SQL query to fetch the specific user
-    // Return user data as an associative array
-}
-?>
